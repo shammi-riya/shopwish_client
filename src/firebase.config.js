1579aaeb757be3d7eb34613bb00e38812e5bafc1
Riya Moni <115640457+shammi-riya@users.noreply.github.com>
2023-08-04 20:48:25 +0600

    add login funtion

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,16 +1,18 @@
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_apiKey,
-  authDomain:import.meta.env.VITE_authDomain,
-  projectId:import.meta.env.VITE_projectId,
-  storageBucket:import.meta.env.VITE_storageBucket,
-  messagingSenderId:import.meta.env.VITE_messagingSenderId,
-  appId:import.meta.env.VITE_appId
+  apiKey: "AIzaSyDEPGu_7n2P_2VAYWhkdKkTHDdaztydM8A",
+  authDomain: "shopwish-376a1.firebaseapp.com",
+  projectId: "shopwish-376a1",
+  storageBucket: "shopwish-376a1.appspot.com",
+  messagingSenderId: "1042437015891",
+  appId: "1:1042437015891:web:bfa08c73338547586cf9a6"
 };
 
-
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
